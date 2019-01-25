005dab194a665410277ebf1eea89bd088debc3e3
vinodc <vinodc@ltpc0gte8t.prounlimited.corp>
2019-01-25 19:03:20 +0530

    committed geko driver changes

--- Jenkins/src/test/java/com/jenkins/Jenkins/AppTest.java
+++ Jenkins/src/test/java/com/jenkins/Jenkins/AppTest.java
@@ -3,23 +3,22 @@ package com.jenkins.Jenkins;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest {
 	@Test
 	public void test1(){
 		WebDriver driver;
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +"/chromedriver/chromedriver");
-		 driver= new ChromeDriver();	
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") +"/chromedriver/geckodriver");
+		 driver= new FirefoxDriver();	
 		driver.get("https://www.toolsqa.com/mobile-automation/appium/appium-tutorial/");
 	}
 	
 	
-	
+
 	
 }
 
